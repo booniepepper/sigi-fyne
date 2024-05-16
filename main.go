88c25bfd5409869b9feed2981192ce04d1a4babb
J.R. Hill <justin@so.dang.cool>
2024-05-16 14:51:34 -0700

    add icon and barebones mockup

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Sigi")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	stack := container.NewGridWithColumns(3,
+		widget.NewLabel("Now"), widget.NewLabel("Make an app"), widget.NewButtonWithIcon("", theme.ConfirmIcon(), func() {}),
+		widget.NewLabel("1"), widget.NewLabel("Take a nap"), widget.NewButtonWithIcon("", theme.ConfirmIcon(), func() {}),
+		widget.NewLabel("2"), widget.NewLabel("Do more stuff"), widget.NewButtonWithIcon("", theme.ConfirmIcon(), func() {}))
+	w.SetContent(stack)
 	w.ShowAndRun()
 }
